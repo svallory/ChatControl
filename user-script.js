bb5bcd916d287273df286373e931d015ea50aa21
Thiago Garcia <thiagogds14@gmail.com>
2015-09-21 14:21:52 -0300

    Bump version to 0.2.

--- user-script.js
+++ user-script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elseve Chat Control Improvements
 // @namespace    http://saulovallory.com/
-// @version      0.1
+// @version      0.2
 // @author       Saulo Vallory
 // @match        *://elsevechatcontrol.dlapp.co/*
 // @grant        none
