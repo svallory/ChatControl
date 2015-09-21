572afdcbecb357c4c93d400ebc2b3d28224d4d9b
Thiago Garcia <thiagogds14@gmail.com>
2015-09-21 16:22:35 -0300

    Add button with link to ticket.

---
+++ whatsapp-user-script.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Whatsapp web Elseve Chat Control Improvements
+// @namespace    http://saulovallory.com/
+// @version      0.1
+// @author       Saulo Vallory
+// @match        *://web.whatsapp.com/*
+// @grant        none
+// ==/UserScript==
+
+var ticketUrl = 'http://elsevechatcontrol.dlapp.co/server/default/ticket/';
+window.$ = function(q) { return document.querySelector(q); }
+window.$$ = function(q) { return document.querySelectorAll(q); }
+
+var bt = document.createElement("button");
+bt.className = "btn btn-primary";
+bt.innerText = "Ticket";
+bt.setAttribute('style', 'position: absolute;right: 115px;top: 12px;');
+
+bt.onclick = function() {
+    var name = $('#main .chat-title > span').innerText;
+    var ticket = (name.match(/[0-9]+/)||[])[0] || false;
+
+    if(ticket){
+        window.open(ticketUrl+ticket, 'ticket-'+ticket);
+    } else {
+        alert('Desculpe, não consegui achar o número do ticket dessa pessoa :(');
+    }
+
+    return false;
+}
+
+window.ticketButton = bt;
+
+function addTicketBtn() {
+    // pane-side  #main
+    var title = $('#main .chat-main');
+    title.appendChild(window.ticketButton);
+}
+
+function startScript(){
+    $('.chatlist').onclick = function(){
+        window.setTimeout(addTicketBtn, 100);
+    }
+}
+
+window.setTimeout(startScript, 6000);
