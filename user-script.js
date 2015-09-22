986e35094f8fd9edf2872875193ed8091c41ced7
Eduardo Carvalho <eduardooc.86@gmail.com>
2015-09-22 19:30:23 -0300

    add handler to open wpw w/ ticket nbr on anchor

--- user-script.js
+++ user-script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elseve Chat Control Improvements
 // @namespace    http://saulovallory.com/
-// @version      0.2
+// @version      0.3
 // @author       Saulo Vallory
 // @match        *://elsevechatcontrol.dlapp.co/*
 // @grant        none
@@ -20,3 +20,21 @@ $.ajaxSetup({
 $('<link rel="stylesheet" href="'+baseUrl+'/dist/ecci.css" />').appendTo('head');
 $.getScript(baseUrl + '/dist/global.js');
 $.getScript(baseUrl + '/dist/ticket.js');
+
+function beginClickHandler () {
+  $('.btn-begin').click(function () {
+    var callNumber = this.parentNode.parentNode.querySelector('td:first-child').innerText;
+    var consumerName = this.parentNode.parentNode.querySelector('td:nth-child(3)').innerText.split(' ')[0];
+    window.open('http://web.whatsapp.com/#' + callNumber + '-' + consumerName, 'WhatsApp Web');
+  });
+};
+
+function startClickHandler () {
+  if(document.querySelector('.btn-begin')) {
+    beginClickHandler();
+    window.clearInterval(beginHandle);
+  }
+}
+
+var beginHandle = window.setInterval(startClickHandler, 1000)
+
