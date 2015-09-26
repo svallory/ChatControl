ceb73fb44e0cab75d905341c44e78e87c968ca2d
Saulo Vallory <me@saulovallory.com>
2015-09-26 13:03:32 -0300

    bump ticket version to 0.3

--- ticket/__header.js
+++ ticket/__header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elseve Chat Control Ticket Improvements
 // @namespace    http://saulovallory.com/
-// @version      0.2
+// @version      0.3
 // @author       Saulo Vallory
 // @match        *://elsevechatcontrol.dlapp.co/server/default/ticket/*
 // @grant        none
